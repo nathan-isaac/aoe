1bedf6dfdadbee5791ff0a8823fca471884c7587
Nathan Isaac <nathan@nathan-isaac.com>
2014-07-13 17:34:37 -0700

    Update environment set method

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function() {
 
-    if(isset($_SERVER['HTTP_HOST']) AND $_SERVER['HTTP_HOST'] == 'aoe.njisaac.com')
+    if(gethostname() == 'web436.webfaction.com')
     {
         return 'production';
     }
