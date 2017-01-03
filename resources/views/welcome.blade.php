8da3d3050f6c567b1377a23d71f3098787769dba
Nathan Isaac <nathan@nathan-isaac.com>
2017-01-02 22:42:19 -0700

    Add footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,4 +27,21 @@
     <teams :civilizations="civilizations" v-show="teamTab"></teams>
     <civilizations :civilizations="civilizations" v-show="!teamTab"></civilizations>
 
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Age of Empires 2 Team Generator</strong> by <a href="http://nathanisaac.io">Nathan Isaac</a>. The source code is licensed
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+                </p>
+                <p>
+                    <a class="icon" href="https://github.com/nisaac2fly/aoe">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </footer>
+
 @endsection
